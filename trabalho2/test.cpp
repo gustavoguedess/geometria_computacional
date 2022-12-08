56f3af1b341c97b304498023e50b7de5af712c21
Gustavo Guedes <gustavoipguedes@gmail.com>
2022-12-08 16:08:47 -0300

    test: funcional dcel test

--- trabalho2/test.cpp
+++ trabalho2/test.cpp
@@ -1,14 +1,38 @@
 
 #include "../geometry/DCEL.h"
+#include <iostream>
+
+using namespace std;
+tDCEL* dcel;
 
 int main(){
     vector<tVertex> vertices;
-    vertices.push_back(tVertex(0, 0, 0));
-    vertices.push_back(tVertex(1, 0, 0));
+    vertices.push_back(tVertex(-1, 1, 0));
+    vertices.push_back(tVertex(-1, -1, 0));
+    vertices.push_back(tVertex(1, -1, 0));
     vertices.push_back(tVertex(1, 1, 0));
-    vertices.push_back(tVertex(0, 1, 0));
-    tDCEL* dcel = new tDCEL(vertices);
+    dcel = new tDCEL(vertices);
     
+    cout << "*** DCEL ***" << endl;
+    print_status(dcel); cout << endl;
+    print_faces(dcel); cout << endl;
+    print_orbits(dcel); cout << endl;
+    cout << endl;
+
+    cout << "*** Add Vertex ***" << endl;
+    dcel->addVertex(tVertex(0, 0, 0));
+    
+    print_status(dcel); cout << endl;
+    print_faces(dcel); cout << endl;
+    print_orbits(dcel); cout << endl;
+
+
+    cout << "*** Add Vertex 2 ***" << endl;
+    dcel->addVertex(tVertex(0.5, 0.5, 0));
+    
+    print_status(dcel); cout << endl;
+    print_faces(dcel); cout << endl;
+    print_orbits(dcel); cout << endl;
     
     return 0;
 }
