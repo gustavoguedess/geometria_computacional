740eec64318cb393e340641114653371ab4cd4ef
Gustavo Guedes <gustavoipguedes@gmail.com>
2022-12-07 21:42:16 -0300

    feat: trabalho2

---
+++ trabalho2/main.cpp
@@ -0,0 +1,180 @@
+/**
+ * @file rectangle2.cpp
+ * Draws a rectangle.
+ *
+ * Draws a rectangle with different colors at each vertex of the composing triangles.
+ * 
+ * @author Ricardo Dutra da Silva
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "../geometry/DCEL.h"
+#include "shaders.h"
+/* Globals */
+/** Window width. */
+int win_width  = 800;
+/** Window height. */
+int win_height = 600;
+
+/** Program variable. */
+int program;
+/** Vertex array object. */
+unsigned int VAO;
+/** Vertex buffer object. */
+unsigned int VBO;
+
+tDCEL *dcel;
+
+/* Functions. */
+void display(void);
+void reshape(int, int);
+void keyboard(unsigned char, int, int);
+void initData(void);
+void initShaders(void);
+
+/** 
+ * Drawing function.
+ *
+ * Draws primitive.
+ */
+void display()
+{
+    	glClearColor(0.2, 0.3, 0.3, 1.0);
+    	glClear(GL_COLOR_BUFFER_BIT);
+
+    	glUseProgram(program);
+    	glBindVertexArray(VAO);
+    	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    	glutSwapBuffers();
+}
+
+/**
+ * Reshape function.
+ *
+ * Called when window is resized.
+ *
+ * @param width New window width.
+ * @param height New window height.
+ */
+void reshape(int width, int height)
+{
+    win_width = width;
+    win_height = height;
+    glViewport(0, 0, width, height);
+    glutPostRedisplay();
+}
+
+
+/** 
+ * Keyboard function.
+ *
+ * Called to treat pressed keys.
+ *
+ * @param key Pressed key.
+ * @param x Mouse x coordinate when key pressed.
+ * @param y Mouse y coordinate when key pressed.
+ */
+void keyboard(unsigned char key, int x, int y)
+{
+        switch (key)
+        {
+                case 27:
+                        glutLeaveMainLoop();
+                case 'q':
+                case 'Q':
+                        glutLeaveMainLoop();
+		case '1':
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			break;
+		case '2':
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			break;
+        }
+    
+	glutPostRedisplay();
+}
+
+/**
+ * Init vertex data.
+ *
+ * Defines the coordinates for vertices, creates the arrays for OpenGL.
+ */
+void initData()
+{
+    vector<tVertex> vertices;
+    vertices.push_back(tVertex(-1.0, 1.0, 0.0));
+    vertices.push_back(tVertex(-1.0, -1.0, 0.0));
+    vertices.push_back(tVertex(1.0, -1.0, 0.0));
+    vertices.push_back(tVertex(1.0, 1.0, 0.0));
+
+    dcel = new tDCEL(vertices);
+
+    
+    // Set triangle vertices.
+    float vertices[] = {
+	// First triangle
+        // coordinate     color
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        // Second triangle
+        // coordinate     color
+         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+         0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f
+    };
+    
+    // Vertex array.
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Vertex buffer
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    // Set attributes.
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // Unbind Vertex Array Object.
+    glBindVertexArray(0);
+}
+
+/** Create program (shaders).
+ * 
+ * Compile shaders and create the program.
+ */
+void initShaders()
+{
+    program = createShaderProgram();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitContextVersion(3, 3);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(win_width,win_height);
+	glutCreateWindow(argv[0]);
+	glewInit();
+
+    	// Init vertex data for the triangle.
+    	initData();
+    
+    	// Create shaders.
+    	initShaders();
+	
+    	glutReshapeFunc(reshape);
+    	glutDisplayFunc(display);
+    	glutKeyboardFunc(keyboard);
+
+	glutMainLoop();
+}
---
+++ trabalho2/shaders.h
@@ -0,0 +1,98 @@
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int createShaderProgram(const char *, const char *);
+
+/** Vertex shader. */
+const char *vertex_code = "\n"
+"#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"layout (location = 1) in vec3 color;\n"
+"\n"
+"out vec3 vColor;\n"
+"\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(position, 1.0);\n"
+"    vColor = color;\n"
+"}\0";
+
+/** Fragment shader. */
+const char *fragment_code = "\n"
+"#version 330 core\n"
+"\n"
+"in vec3 vColor;\n"
+"out vec4 FragColor;\n"
+"\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(vColor, 1.0f);\n"
+"}\0";
+
+/** 
+ * Create program.
+ *
+ * Creates a program from given shader codes.
+ *
+ * @param vertex_code String with code for vertex shader.
+ * @param fragment_code String with code for fragment shader.
+ * @return Compiled program.
+ */
+int createShaderProgram()
+{
+	
+    int success;
+    char error[512];
+
+    // Request a program and shader slots from GPU
+    int program  = glCreateProgram();
+    int vertex   = glCreateShader(GL_VERTEX_SHADER);
+    int fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    
+    // Set shaders source
+    glShaderSource(vertex, 1, &vertex_code, NULL);
+    glShaderSource(fragment, 1, &fragment_code, NULL);
+
+    // Compile shaders
+    glCompileShader(vertex);
+    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+	glGetShaderInfoLog(vertex, 512, NULL, error);
+	std::cout << "ERROR: Shader comilation error: " << error << std::endl;
+    }
+                
+    glCompileShader(fragment);
+    glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+	glGetShaderInfoLog(fragment, 512, NULL, error);
+	std::cout << "ERROR: Shader comilation error: " << error << std::endl;
+    }
+
+    // Attach shader objects to the program
+    glAttachShader(program, vertex);
+    glAttachShader(program, fragment);
+
+    // Build program
+    glLinkProgram(program);
+    glGetShaderiv(program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+	glGetProgramInfoLog(program, 512, NULL, error);
+	std::cout << "ERROR: Program link error: " << error << std::endl;
+    }
+
+    // Get rid of shaders (not needed anymore)
+    glDetachShader(program, vertex);
+    glDetachShader(program, fragment);
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+
+    return program;
+}
